150a0744767dd6d421045a20e27ac69856cd7bce
MartinekV <martinekvlastimil95@gmail.com>
2025-02-25 10:41:22 +0100

    Add wandb logging

--- src/playground.py
+++ src/playground.py
@@ -1,21 +1,41 @@
 import os
 import dotenv
-from smolagents import CodeAgent, LiteLLMModel, tool
-from telemetry_setup import logging_setup
+from smolagents import CodeAgent, LiteLLMModel, ToolCallingAgent
+from tools.bash import bash_tool
+import wandb
 
 dotenv.load_dotenv()
+wandb.login(key=os.getenv("WANDB_API_KEY"))
 api_key = os.getenv("OPENAI_API_KEY")
-logging_setup()
 
-models = ["gpt-4o-mini-2024-07-18",'gpt-4o-2024-08-06'] 
+model = "gpt-4o-mini-2024-07-18"
+wandb.init(
+    entity="ceitec-ai",
+    project="BioAgents",
+    tags=["testing"],
+    config={
+        "model":model,
+    }
+)
 
-model = LiteLLMModel(model_id=models[0], api_key=api_key, temperature=0)
-agent = CodeAgent(
-    tools=[], 
+model = LiteLLMModel(
+    model_id=model, 
+    api_key=api_key, 
+    temperature=1,
+)
+
+agent = ToolCallingAgent(
+    tools=[bash_tool], 
     model=model, 
     add_base_tools=False,
-    additional_authorized_imports=["*"],
     max_steps=5,
 )
 
-agent.run('What is sqrt of 5**5')
+agent.run("What packages are installed in the multiagent-ml-env conda environment?")
+
+wandb.log(
+    {
+        "Agent F1 score": 0.5087987, 
+    }
+)
+
