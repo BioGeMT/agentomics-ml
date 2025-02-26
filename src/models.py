86cd9f0b3de72fdea3fcf025de83fec4a7f0581c
MartinekV <martinekvlastimil95@gmail.com>
2025-02-26 12:07:04 +0100

    Extract model selection and expand playground script

---
+++ src/models.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class StrEnum(str, Enum):
+    def __str__(self):
+        return self.value
+    
+    def __repr__(self):
+        return self.value
+
+class MODELS(StrEnum):
+    GPT4o="gpt-4o-2024-08-06"
+    GPT4o_mini="gpt-4o-mini-2024-07-18"
+    OPENROUTER_GPT4o="openrouter/openai/gpt-4"
+    OPENROUTER_SONNET_37="openrouter/anthropic/claude-3-7-sonnet-20250219"
