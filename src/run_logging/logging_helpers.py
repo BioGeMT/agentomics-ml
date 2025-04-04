e8c6b87c6c221610391a1d42085e5e084bd7a027
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2025-04-04 12:19:09 +0200

    Refactor logging functions: replace log_inference_stage with log_inference_stage_and_metrics and remove logging_fn parameter

---
+++ src/run_logging/logging_helpers.py
@@ -0,0 +1,12 @@
+import wandb
+
+def log_inference_stage_and_metrics(stage, metrics=None):
+    wandb.log({"inference_stage": stage})
+
+    if stage == 0 or stage == 1:
+        wandb.log({
+            'AUPRC': -1,
+            'AUROC': -1,
+        })
+    else:
+        wandb.log(metrics)
