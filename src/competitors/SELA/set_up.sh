da32d0b73eab2fb8bc1d5c23c2cb3bdb87bf24e8
David Cechak <davidcechak@gmail.com>
2025-05-08 13:23:44 +0100

    sela: finish set up, run is in progress

--- src/competitors/SELA/set_up.sh
+++ src/competitors/SELA/set_up.sh
@@ -1,10 +1,35 @@
-# git submodule add https://github.com/davidcechak/MetaGPT.git
+conda create -p /tmp/sela_env python=3.9 -y
+source /opt/conda/etc/profile.d/conda.sh
+# conda activate "$AGENT_ENV"
+conda activate /tmp/sela_env
+# pip install --upgrade metagpt #use --no-cache-dir in case of problems
 git pull https://github.com/davidcechak/MetaGPT.git
-# instead of agentomics yaml we PROBABLY only need the one-shot yaml 
-# conda env create -n agentomics_sela_tmp -f environment.yaml
-conda env create -n agentomics_sela_tmp -f ../1-shot_llm/environment.yaml
-conda activate agentomics_sela_tmp
 cd MetaGPT
 pip install -e .
 cd metagpt/ext/sela
 pip install -r requirements.txt
+
+pip install agentops==0.4.9 #Fix metagpt env error
+
+pip install wandb
+pip install python-dotenv
+pip install pyyaml
+pip install hrid==0.2.4
+pip install pandas
+pip install scikit-learn
+# This is needed for SELA
+pip install openml
+
+# mkdir "$AGENT_DIR"/DI
+# cd "$AGENT_DIR"/DI
+# metagpt --init-config
+mkdir /tmp/sela
+cd /tmp/sela
+metagpt --init-config
+
+cp /repository/src/competitors/SELA/run.py /tmp/sela/run.py
+cp /repository/src/competitors/SELA/set_config.py /tmp/sela/set_config.py
+
+# Give the run permission to write to the DI directory and env
+chmod -R 777 /tmp/sela
+chmod -R 777 /tmp/sela_env
