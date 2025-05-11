dc7d39dad9107ad663ac3f47a41df36855a58fcd
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2025-05-11 23:06:05 +0000

    Edit prompt to suggest GPU usage

--- src/steps/final_outcome.py
+++ src/steps/final_outcome.py
@@ -9,6 +9,7 @@ def get_final_outcome_prompt():
     return """
     Next task: create inference.py file.
     The inference script will be taking the following named arguments:
+    If your model can be accelerated by GPU, implement the code to use GPU.
     --input (an input file path, file is of the same format as your training data (except the target column))
     --output (the output file path, this file should be a one column csv file with the predictions containing a score from 0 to 1, the column name should be 'prediction')
     """
