51591afd7f489c805967b9559da12dd3b4d34907
Dimos Tzimotoudis <dtzimotoudis@gmail.com>
2025-10-28 23:25:48 +0000

    dummy grader

---
+++ competitors/templates/grade_template.py
@@ -0,0 +1,5 @@
+"""Dummy grader for Agentomics tasks - not used, we do our own evaluation"""
+
+def grade(submission, answers):
+    """Dummy grading function required by BioMLBench but never called during run-agent"""
+    return 0.0
