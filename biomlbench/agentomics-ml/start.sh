e59a5f4ca48dc45c53cec0a8348cb0d431edfa4e
MartinekV <martinekvlastimil95@gmail.com>
2025-12-01 13:04:38 +0000

    Update agentomics pull path for biomlbench

--- biomlbench/agentomics-ml/start.sh
+++ biomlbench/agentomics-ml/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-git clone -b biomlbench_agentomics_implementation https://github.com/BioGeMT/agentomics-ml.git /home/agentomics-ml
+git clone https://github.com/BioGeMT/agentomics-ml.git /home/agentomics-ml
 
 cp -r /home/agentomics-ml/src /home/agent/src
 
