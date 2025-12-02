3d8eecb2723767baf222599437b011049d1f45a8
MartinekV <martinekvlastimil95@gmail.com>
2025-12-02 12:25:16 +0000

    Update orchestrator

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+
+REPOS_DIR="/home/$USER/repos" #this needs to be configured to the agentomics repository parent directory (biomlbench will be pulled as a sibling to the agentomics repo)
+
+SPEND_LIMIT=10
 MODELS=("openai/gpt-5" "openai/gpt-5-codex" "anthropic/claude-sonnet-4.5" "anthropic/claude-haiku-4.5")
-DATASETS=("AGO2_CLASH_Hejret")
-ITERATIONS=100
-TIME_BUDGET_S=$(( 8 * 60 * 60 )) # 8 hours
+ITERATIONS=100 # Set to a large number because timeout will take precedence anyways
+TIME_BUDGET_S=$(( 8 * 60 * 60 )) # 8 hours, biomlbench datasets are set to 8h automatically and will not react to this
 SPLIT_ALLOWED_ITERS=4
 TAGS=("experiment_orchestrator" "test_run")
 REPETITIONS=2
+USER_PROMPT="Create a machine learning model that will generalize to new unseen data."
 
-DATASETS_VAL_METRICS=(
+GENOMIC_DATASETS=("AGO2_CLASH_Hejret")
+BIOMLBENCH_DATASETS=(
+  "polarishub/polaris-pkis2-egfr-wt-c-1"
+  "polarishub/polaris-adme-fang-hclint-1"
+  "polarishub/polaris-adme-fang-hppb-1"
+  "polarishub/polaris-adme-fang-solu-1"
+  "polarishub/tdcommons-cyp2d6-substrate-carbonmangels"
+  "polarishub/tdcommons-lipophilicity-astrazeneca"
+  "polarishub/tdcommons-herg"
+  "polarishub/tdcommons-bbb-martins"
+  "polarishub/tdcommons-caco2-wang"
+  "proteingym-dms/SPIKE_SARS2_Starr_2020_binding"
+  "proteingym-dms/SPA_STAAU_Tsuboyama_2023_1LP1"
+  "proteingym-dms/PSAE_PICP2_Tsuboyama_2023_1PSE_indels"
+  "proteingym-dms/CBX4_HUMAN_Tsuboyama_2023_2K28"
+  "proteingym-dms/Q8EG35_SHEON_Campbell_2022_indels"
+  "proteingym-dms/CSN4_MOUSE_Tsuboyama_2023_1UFM_indels"
+)
+GENOMIC_DATASETS_VAL_METRICS=(
     "AGO2_CLASH_Hejret:AUPRC"
     "human_enhancers_cohn:ACC"
     "human_enhancers_ensembl:ACC"
@@ -16,23 +38,42 @@ DATASETS_VAL_METRICS=(
 )
 
 declare -A metric_map
-  for config in "${DATASETS_VAL_METRICS[@]}"; do
+  for config in "${GENOMIC_DATASETS_VAL_METRICS[@]}"; do
       dataset="${config%%:*}"
       metric="${config##*:}"
       metric_map["$dataset"]="$metric"
   done
 
 for repetition in $(seq 1 $REPETITIONS); do
-    for dataset in "${DATASETS[@]}"; do
+    for dataset in "${GENOMIC_DATASETS[@]}"; do
         for model in "${MODELS[@]}"; do
             ./run.sh \
                 --model "$model" \
                 --dataset "$dataset" \
                 --iterations "$ITERATIONS" \
+                --use-provisioning-key \
+                --spend-limit "$SPEND_LIMIT" \
+                --user-prompt "$USER_PROMPT" \
                 --split-allowed-iterations "$SPLIT_ALLOWED_ITERS" \
                 --val-metric "${metric_map[$dataset]}" \
                 --tags "${TAGS[@]}" \
                 --timeout "$TIME_BUDGET_S"
+        done
+    done
+done
+
+for repetition in $(seq 1 $REPETITIONS); do
+    for dataset in "${BIOMLBENCH_DATASETS[@]}"; do
+        for model in "${MODELS[@]}"; do
+            ./biomlbench/run_benchmarks.sh \
+                --repos-dir "$REPOS_DIR" \
+                --spend-limit "$SPEND_LIMIT" \
+                --dset "$dataset" \
+                --iterations "$ITERATIONS" \
+                --split-allowed-iterations "$SPLIT_ALLOWED_ITERS" \
+                --model "$model" \
+                --user-prompt "$USER_PROMPT" \
+                --tags "${TAGS[@]}"
         done
     done
 done
