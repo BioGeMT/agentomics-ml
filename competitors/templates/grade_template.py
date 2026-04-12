38ec436c3c4dcca4b9b9ab3870ee9b5914bba67b
MartinekV <martinekvlastimil95@gmail.com>
2026-04-13 00:44:32 +0200

    Remove biomlbench, competitors, and compute_stealth_test external tooling

--- competitors/templates/grade_template.py
+++
@@ -1,5 +0,0 @@
-"""Dummy grader for Agentomics tasks - not used, we do our own evaluation"""
-
-def grade(submission, answers):
-    """Dummy grading function required by BioMLBench but never called during run-agent"""
-    return 0.0
