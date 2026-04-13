455ef22fc84ee72c57edc27782598587f3486d2b
MartinekV <martinekvlastimil95@gmail.com>
2026-04-13 19:28:12 +0200

    Move generate_final_reports.py into src/runtime/

--- run.sh
+++ run.sh
@@ -394,7 +394,7 @@ if [ "$LOCAL_MODE" = true ]; then
         fi
 
         write_outputs_readme "${AGENT_ID}"
-        conda run -n agentomics-env python src/generate_final_reports.py \
+        conda run -n agentomics-env python src/runtime/generate_final_reports.py \
             --agent-dir "outputs/${AGENT_ID}" \
             --prepared-datasets $(pwd)/prepared_datasets \
             --prepared-tests $(pwd)/prepared_test_sets
@@ -640,7 +640,7 @@ else
               -v "$(pwd)":/repository \
               -v "$(pwd)/outputs/${AGENT_ID}":/agent_out \
               --entrypoint /opt/conda/envs/agentomics-env/bin/python \
-              "$AGENTOMICS_IMAGE" /repository/src/generate_final_reports.py \
+              "$AGENTOMICS_IMAGE" /repository/src/runtime/generate_final_reports.py \
                 --agent-dir /agent_out --prepared-datasets /repository/prepared_datasets \
                 --prepared-tests /repository/prepared_test_sets
 
